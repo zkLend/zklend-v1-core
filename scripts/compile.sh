96107d22047b7d46cc51aca780298fda35983c3d
Jonathan LEI <me@xjonathan.dev>
2022-05-22 16:56:01 +0000

    chore: add script for compiling contracts

---
+++ scripts/compile.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+set -e
+
+SCRIPT_DIR=$( cd -- "$( dirname "$0" )" &> /dev/null && pwd )
+REPO_ROOT=$( cd -- "$( dirname $( dirname "$0" ) )" &> /dev/null && pwd )
+
+compile () {
+  SOURCE="$1"
+  SOURCE_DIR="$(dirname "$SOURCE")"
+  SOURCE_FILE="$(basename -- "$SOURCE")"
+  OUTPUT_DIR="../build/$SOURCE_DIR"
+  OUTPUT="$OUTPUT_DIR/${SOURCE_FILE%.*}.json"
+
+  echo "Compiling $(realpath $SOURCE)"
+
+  mkdir -p "$OUTPUT_DIR"
+
+  # Ignores debug info for smaller artifacts
+  starknet-compile --no_debug_info $SOURCE > $OUTPUT
+
+  if [ -n "$USER_ID" ] && [ -n "$GROUP_ID" ]; then
+    chown $USER_ID:$GROUP_ID $OUTPUT_DIR
+    chown $USER_ID:$GROUP_ID $OUTPUT
+  fi
+}
+
+cd "$REPO_ROOT/src"
+mkdir -p "$REPO_ROOT/build/zklend"
+
+find -type f -name '*.cairo' | while read SOURCE; do
+  compile "$SOURCE"
+done
+
+if [ -n "$USER_ID" ] && [ -n "$GROUP_ID" ]; then
+  chown -R $USER_ID:$GROUP_ID "$REPO_ROOT/build"
+fi
---
+++ scripts/compile_with_docker.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+SCRIPT_DIR=$( cd -- "$( dirname "$0" )" &> /dev/null && pwd )
+REPO_ROOT=$( cd -- "$( dirname $( dirname "$0" ) )" &> /dev/null && pwd )
+
+# Deterministically generate contract artifacts
+docker run -it --rm \
+    -v "$REPO_ROOT:/work" \
+    --env "USER_ID=$(id -u)" \
+    --env "GROUP_ID=$(id -g)" \
+    --entrypoint sh \
+    shardlabs/cairo-cli:0.8.2.1 \
+    -c "cd /work && ./scripts/compile.sh"
+
+# Using prettier instead of `jq` due to known issue:
+#   https://github.com/xJonathanLEI/starknet-rs/issues/76#issuecomment-1058153538
+docker run -it --rm \
+    -v "$REPO_ROOT/build:/work" \
+    --user root \
+    tmknom/prettier:2.6.2 \
+    --write .
