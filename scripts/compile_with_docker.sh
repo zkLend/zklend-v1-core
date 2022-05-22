89b941886fdc6dd5c9c2bd996fe7fb8c0ef0b34f
Jonathan LEI <me@xjonathan.dev>
2022-05-22 17:02:06 +0000

    ci: add GitHub Actions workflow for building contracts

--- scripts/compile_with_docker.sh
+++ scripts/compile_with_docker.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
+set -e
+
 SCRIPT_DIR=$( cd -- "$( dirname "$0" )" &> /dev/null && pwd )
 REPO_ROOT=$( cd -- "$( dirname $( dirname "$0" ) )" &> /dev/null && pwd )
 
 # Deterministically generate contract artifacts
-docker run -it --rm \
+docker run --rm \
     -v "$REPO_ROOT:/work" \
     --env "USER_ID=$(id -u)" \
     --env "GROUP_ID=$(id -g)" \
@@ -14,7 +16,7 @@ docker run -it --rm \
 
 # Using prettier instead of `jq` due to known issue:
 #   https://github.com/xJonathanLEI/starknet-rs/issues/76#issuecomment-1058153538
-docker run -it --rm \
+docker run --rm \
     -v "$REPO_ROOT/build:/work" \
     --user root \
     tmknom/prettier:2.6.2 \
