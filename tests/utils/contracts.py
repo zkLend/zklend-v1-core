4fc5492b91b86e5837db4c097f7fb3fdc1a7ed96
Jonathan LEI <me@xjonathan.dev>
2022-08-17 06:06:25 +0000

    chore: remove ZigZag oracle code

--- tests/utils/contracts.py
+++ tests/utils/contracts.py
@@ -10,9 +10,6 @@ CAIRO_PATH = str(SRC_ROOT)
 PATH_MARKET = str(CONTRACT_ROOT / "Market.cairo")
 PATH_ZTOKEN = str(CONTRACT_ROOT / "ZToken.cairo")
 PATH_PROXY = str(CONTRACT_ROOT / "Proxy.cairo")
-PATH_ZIG_ZAG_ORACLE_ADAPTER = str(
-    CONTRACT_ROOT / "oracles" / "ZigZagOracleAdapter.cairo"
-)
 PATH_DEFAULT_INTEREST_RATE_MODEL = str(
     CONTRACT_ROOT / "irms" / "DefaultInterestRateModel.cairo"
 )
@@ -26,7 +23,6 @@ PATH_ERC20_MINTABLE = str(
 PATH_MOCK_SAFE_CAST = str(TESTS_ROOT / "mocks" / "SafeCast_mock.cairo")
 PATH_MOCK_SAFE_MATH = str(TESTS_ROOT / "mocks" / "SafeMath_mock.cairo")
 PATH_MOCK_SAFE_DECIMAL_MATH = str(TESTS_ROOT / "mocks" / "SafeDecimalMath_mock.cairo")
-PATH_MOCK_ZIG_ZAG_ORACLE = str(TESTS_ROOT / "mocks" / "MockZigZagOracle.cairo")
 PATH_MOCK_MATH = str(TESTS_ROOT / "mocks" / "Math_mock.cairo")
 PATH_MOCK_PRICE_ORACLE = str(TESTS_ROOT / "mocks" / "MockPriceOracle.cairo")
 PATH_MOCK_MARKET = str(TESTS_ROOT / "mocks" / "MockMarket.cairo")
